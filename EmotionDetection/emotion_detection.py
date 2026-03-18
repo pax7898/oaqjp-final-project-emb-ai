46722f3ae3d96130644f826a975c3c4fc2f5e245
Pax7898 <pasqualecaggiano007@gmail.com>
2026-03-18 06:37:18 -0400

    Finish project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,8 +3,20 @@ import requests
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    obj_json = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url, json = obj_json, headers=header)
+    obj_json = {"raw_document": {"text": text_to_analyze}}
+
+    response = requests.post(url, json=obj_json, headers=header)
+
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     formatted_response = response.json()
     emotions = formatted_response['emotionPredictions'][0]['emotion']
     emotions['dominant_emotion'] = max(emotions, key=emotions.get)
---
+++ server.py
@@ -0,0 +1,29 @@
+from EmotionDetection.emotion_detection import emotion_detector
+from flask import Flask, request, render_template
+
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def emo_detector():
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
+    return (
+        "For the given statement, the system response is "
+        f"'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. "
+        f"The dominant emotion is {response['dominant_emotion']}."
+    )
+
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
